1ced9f8b7496b432a02ca295e2c243be313e42c2
Bijan Dayani <bdayani518@gmail.com>
2020-02-22 15:33:34 -0500

    import navbar and users components, rmv h1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <h1>Hello World</h1>
-      </Fragment>
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
     );
   }
 }
