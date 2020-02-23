7e5d22faa810693c6630fd707d1351a262f88ff8
Bijan Dayani <bdayani518@gmail.com>
2020-02-23 18:20:45 -0500

    api request for users repos, add to state

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -35,6 +36,16 @@ class App extends Component {
     );
 
     this.setState({ user: res.data, loading: false });
+  };
+
+  // GET User's Repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
   };
 
   // clear users from state
