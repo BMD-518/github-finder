f3ee36238eb3885b4c557bcd5865b3d31537b9fd
Bijan Dayani <bdayani518@gmail.com>
2020-02-23 00:23:07 -0500

    getUser() + user to state

--- src/App.js
+++ src/App.js
@@ -11,18 +11,29 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
 
+  // Search Github Users
   searchUsers = async text => {
     this.setState({ loading: true });
-    console.log(text);
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ users: res.data.items, loading: false });
+  };
+
+  // GET Single User
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
   };
 
   // clear users from state
