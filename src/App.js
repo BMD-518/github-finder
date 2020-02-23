c968034f1617efabeac3b93b13bdd384616976fb
Bijan Dayani <bdayani518@gmail.com>
2020-02-22 21:54:51 -0500

    search and clear users

--- src/App.js
+++ src/App.js
@@ -11,22 +11,25 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({ loading: true });
-
+    console.log(text);
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
