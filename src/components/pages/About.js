613dd7bbb891d906462fa66fb46a98557dc24a60
Bijan Dayani <bdayani518@gmail.com>
2020-02-27 17:29:52 -0500

    test form component using tailwind

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import TailwindTest from '../practice/TailwindTest';
 
 export const About = () => {
   return (
@@ -6,6 +7,7 @@ export const About = () => {
       <h1>About This App</h1>
       <p>App to search Github users</p>
       <p>Version 1.0.0</p>
+      <TailwindTest />
     </Fragment>
   );
 };
