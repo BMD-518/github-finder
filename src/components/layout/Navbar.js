7c3b16b6105935d9c6e2483ec2516d4ad76835a0
Bijan Dayani <bdayani518@gmail.com>
2020-03-01 21:06:27 -0500

    rough tailwind navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
-    <header className='flex-col px-6 bg-gray-900 flex items-center lg:py-0 py-2'>
-      <div className='items-center text-3xl text-green-500 font-semibold pb-1'>
-        {title}
-      </div>
-      <div className='flex'>
-        <p className='px-10 text-green-200 hover:text-green-500'>
-          <Link to='/'>Home</Link>
-        </p>
-        <p className='px-10 text-green-200 hover:text-green-500'>
-          <Link to='/about'>About</Link>
-        </p>
-      </div>
-    </header>
+    <Fragment>
+      <header className='flex-col px-6 bg-gray-900 flex items-center lg:py-0 py-2'>
+        <div className='items-center text-3xl text-green-500 font-semibold pb-1 tracking-widest'>
+          {title}
+        </div>
+        <div className='flex'>
+          <p className='px-10 text-green-200 hover:text-green-500 tracking-wider'>
+            <Link to='/'>Home</Link>
+          </p>
+          <p className='px-10 text-green-200 hover:text-green-500 tracking-wider'>
+            <Link to='/about'>About</Link>
+          </p>
+        </div>
+      </header>
+      <div className='bg-green-500 pt-1 w-full'></div>
+    </Fragment>
   );
 };
 
