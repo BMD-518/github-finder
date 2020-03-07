4eebd9acf106762bcafbc94b01ccef28eaef14a8
Bijan Dayani <bdayani518@gmail.com>
2020-03-07 17:01:35 -0500

    button/cardish layout

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -3,10 +3,22 @@ import PropTypes from 'prop-types';
 
 const RepoItem = ({ repo }) => {
   return (
-    <div className='card'>
-      <h3>
+    <div className='flex flex-col p-3 mb-2 rounded-md bg-gray-600'>
+      <button
+        className='
+      flex justify-center 
+      focus:outline-none 
+      rounded-sm 
+      my-5 py-2 px-4 
+      bg-gray-700 
+      border-b-2 border-green-500 
+      font-semibold 
+      text-green-500 uppercase
+      hover:bg-green-500 hover:text-green-900
+      '
+      >
         <a href={repo.html_url}>{repo.name}</a>
-      </h3>
+      </button>
     </div>
   );
 };
