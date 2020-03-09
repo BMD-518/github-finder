9408e1c4fb595cb1eda00387c0ff87fdd9ae8b23
Bijan Dayani <bdayani518@gmail.com>
2020-03-08 23:29:16 -0400

    card, font changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,14 +1,12 @@
-import React, { Fragment } from 'react';
-import TailwindTest from '../practice/TailwindTest';
+import React from 'react';
 
 export const About = () => {
   return (
-    <Fragment>
-      <h1>About This App</h1>
-      <p>App to search Github users</p>
+    <div className='m-5 p-5 text-center rounded-md bg-gray-600 hover:bg-gray-500 hover:shadow-2xl'>
+      <h1 className='text-green-300 font-thin text-3xl'>About This App</h1>
+      <p className='text-lg'>App to search Github users</p>
       <p>Version 1.0.0</p>
-      <TailwindTest />
-    </Fragment>
+    </div>
   );
 };
 
