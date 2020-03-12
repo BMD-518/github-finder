ef4542657d615a9841e17cfd17ae66a3dd396506
Bijan Dayani <bdayani518@gmail.com>
2020-03-11 21:02:22 -0400

    purgecss templates, extractor && run on production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 // postcss configuration
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.html', './src/**/*.js', './public/index.html'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
 
 module.exports = {
-  plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')]
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ]
 };
