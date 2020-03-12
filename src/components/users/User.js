cf4b9961ac260ac2b6fbd789c91c764e2f7ad74f
Bijan Dayani <bdayani518@gmail.com>
2020-03-11 23:48:34 -0400

    mobile view bug, set img width&height

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -54,7 +54,7 @@ const User = ({ match }) => {
           <div className='flex flex-wrap justify-center'>
             <img
               src={avatar_url}
-              className='mb-3 rounded-full border-green-400 border-4 shadow-xl'
+              className='w-40 h-40 mb-3 rounded-full border-green-400 border-4 shadow-xl'
               alt='{name}'
             />
           </div>
